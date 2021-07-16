da1472a4e8706725219ef311b36a8b1123676f22
Ibrahim Faruqi <izf@izfaruqi.com>
2021-07-17 03:07:13 +0700

    Add download all media feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('./config.json')
 const puppeteer = require('puppeteer-core')
 const fs = require('fs').promises
+const fsSync = require('fs')
 const _ = require('lodash')
 const axios = require('axios').default
 
@@ -9,7 +10,7 @@ async function main() {
   const meta = {}
   meta.scrape_ids_start = Date.now()
   const parentUrl = process.argv[2]
-  if(parentUrl && (parentUrl.length <= 0)){
+  if (parentUrl && (parentUrl.length <= 0)) {
     console.log('no url provided')
     return
   }
@@ -40,17 +41,17 @@ async function main() {
         clearInterval(window.threadScroller)
         window.threadRefreshObserver.disconnect()
       })
-      if(debug) console.log('scroller stopped')
+      if (debug) console.log('scroller stopped')
       tweetIds = _.uniq(tweetIds).map(link => link.split('/')[3]).sort()
       resolve(tweetIds)
     }
 
     await page.exposeFunction('sendLinkToScrolledThread', async link => {
-      if(!link.startsWith('/' + username)){
-        if(debug) console.log('tweet from different username found: ' + link)
+      if (!link.startsWith('/' + username)) {
+        if (debug) console.log('tweet from different username found: ' + link)
         await finish()
       } else {
-        if(debug) console.log(link)
+        if (debug) console.log(link)
         tweetIds.push(link)
       }
     })
@@ -77,29 +78,67 @@ async function main() {
   meta.scrape_ids_stop = Date.now()
 
   const grabTweetDataFromApi = async (tweetIds, debug = false) => {
-    if(debug) console.log('fetching tweet data from api')
+    if (debug) console.log('fetching tweet data from api')
 
     let tweetIdsChunks = [tweetIds]
-    if(tweetIds.length > 5){
+    if (tweetIds.length > 5) {
       tweetIdsChunks = _.chunk(tweetIds, 5)
     }
 
-    return _.merge(...(await Promise.allSettled(tweetIdsChunks.map(tweetIdChunks => 
+    return _.merge(...(await Promise.allSettled(tweetIdsChunks.map(tweetIdChunks =>
       axios.get('https://api.twitter.com/1.1/statuses/lookup.json?id=' + encodeURIComponent(tweetIdChunks.join(',')) + "&map=true&include_ext_alt_text=true",
         { headers: { 'Authorization': 'Bearer ' + config.twitterApiToken } }).then(res => res.data.id)
-    ))).map(promise => promise.status == 'fulfilled'? promise.value : null).flat())
+    ))).map(promise => promise.status == 'fulfilled' ? promise.value : null).flat())
   }
-  
+
   meta.grab_tweet_data_start = Date.now()
   const tweetData = await grabTweetDataFromApi(tweetIds, true)
   meta.grab_tweet_data_end = Date.now()
 
-  await fs.writeFile('output/' + username + '_' + splitParentUrl[5] + '_' + meta.scrape_ids_start + '.json', JSON.stringify({
+  const mediaUrls = Object.values(tweetData).map(tweet => {
+    try {
+      if (tweet.extended_entities.media.length > 0) {
+        return tweet.extended_entities.media.map(media => {
+          if (media.type == 'video') { // Video
+            return { id: media.id_str, url: media.video_info.variants[media.video_info.variants.length - 1].url }
+          } else { // Photo
+            return { id: media.id_str, url: media.media_url + '?name=orig' }
+          }
+        })
+      }
+    } catch (e) {
+      return null
+    }
+    return null
+  }).filter(x => x != null).flat()
+
+  meta.media = mediaUrls
+
+  const outputFileName = username + '_' + splitParentUrl[5] + '_' + meta.scrape_ids_start
+
+  await fs.mkdir('output/' + outputFileName)
+  await fs.writeFile('output/' + outputFileName + "/" + outputFileName + '.json', JSON.stringify({
     ...meta,
     tweet_data: tweetData
   }))
   console.log('tweet data dumped to json')
 
+  console.log(mediaUrls)
+
+  console.log('downloading media')
+  if (mediaUrls.length > 0) {
+    fs.mkdir('output/' + outputFileName + '/media')
+    await Promise.allSettled(mediaUrls.map(media => axios.get(media.url, { responseType: 'stream' }).then(res => {
+      const urlSplit = (new URL(media.url)).pathname.split('/')
+      const savePath = 'output/' + outputFileName + '/media/' + media.id + '-' + urlSplit[urlSplit.length - 1]
+      res.data.pipe(fsSync.createWriteStream(savePath))
+      return new Promise((res, rej) => {
+        res.data.on('end', res)
+        res.data.on('error', rej)
+      })
+    })))
+  }
+  console.log('media downloaded')
   await browser.close()
 }
 
