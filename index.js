aba972fc705d19cb5366bcf40be499917af73991
Ibrahim Faruqi <izf@izfaruqi.com>
2021-07-19 15:09:14 +0700

    Properly parse short-no-replies thread and only download user data once

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ async function main() {
   const splitParentUrl = parentUrl.split('/')
   const username = splitParentUrl[3]
   meta.url = parentUrl
-  meta.user = {
-    screen_name: username
-  }
 
-  const browser = await puppeteer.launch({ executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", userDataDir: './puppeteer-userdata', headless: false })
+  const browser = await puppeteer.launch({ executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", userDataDir: './puppeteer-userdata', headless: true })
   const page = await browser.newPage()
   page.setViewport({ width: 800, height: 850 })
   page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
@@ -33,7 +30,16 @@ async function main() {
 
   const upperTweets = await page.$$('article > div > div > div > div:last-child > div:last-child > div:first-child > div > div > div:first-child > a:last-child')
   const upperTweetLinks = await Promise.all(upperTweets.map(async article => (await (await article.getProperty('href')).jsonValue())))
+
+  await page.waitForSelector('section > div > div')
+  await page.waitForSelector('section > div > div')
   
+  const isThreadSeparatorDetected = await page.evaluate(() => {
+    return Array.from(document.querySelector('section > div > div').querySelectorAll(':scope > div')).filter(e => e.textContent == "").length >= 2
+  })
+
+  console.log(isThreadSeparatorDetected)
+
   let upperLimitIdx = -1
   upperTweetLinks.forEach((link, i) => (!link.startsWith('https://twitter.com/' + username) && upperLimitIdx == -1)? upperLimitIdx = i : null)
   const upperTweetIds = upperTweetLinks.filter((_, i) => !(upperLimitIdx != -1 && i >= upperLimitIdx)).map(link => link.split('/')[5])
@@ -42,11 +48,12 @@ async function main() {
   tweetIds.push(...upperTweetIds)
 
   // If the upper tweets haven't encountered a thread-breaking filter and is not a single tweet, continue grabbing the lower tweets.
-  if(upperLimitIdx == -1 && upperTweetIds.length != 0){
+  if(upperLimitIdx == -1 && upperTweetIds.length != 0 & !isThreadSeparatorDetected){
     const grabLowerTweetIds = (debug = false) => new Promise(async resolve => {
       let tweetIds = []
   
       const finish = async () => {
+        console.log('thread seperator found.')
         await page.evaluate(() => {
           clearInterval(window.threadScroller)
           window.threadRefreshObserver.disconnect()
@@ -58,21 +65,28 @@ async function main() {
       }
   
       await page.exposeFunction('sendLinkToScrolledThread', async link => {
-        if (!link.startsWith('/' + username)) {
-          if (debug) console.log('tweet from different username found: ' + link)
-          await finish()
-        } else {
+        if(link.startsWith('/' + username)){
           if (debug) console.log(link)
           tweetIds.push(link)
         }
       })
+
+      await page.exposeFunction('finishScrollThread', async _ => {
+        await finish()
+      })
   
       await page.evaluate(() => {
         const threadContainer = document.querySelector('section > div > div')
+        window.finishing = false
         window.threadRefreshObserver = new MutationObserver(muts => {
           for (const mut of muts) {
             if (mut.addedNodes.length >= 1) {
-              sendLinkToScrolledThread(mut.addedNodes[0].querySelector('article > div > div > div > div:last-child > div:last-child > div:first-child > div > div > div:first-child > a:last-child').getAttribute('href'))
+              if(mut.addedNodes[0].textContent != ""){ // Stop if detected a thread seperator.
+                sendLinkToScrolledThread(mut.addedNodes[0].querySelector('article > div > div > div > div:last-child > div:last-child > div:first-child > div > div > div:first-child > a:last-child').getAttribute('href'))
+              } else if(!window.finising) {
+                window.finishing = true
+                finishScrollThread()
+              }
             }
           }
         })
@@ -87,6 +101,7 @@ async function main() {
   }
 
   console.log(tweetIds)
+  meta.tweetIds = tweetIds
   meta.scrape_ids_stop = Date.now()
 
   const grabTweetDataFromApi = async (tweetIds, debug = false) => {
@@ -100,7 +115,7 @@ async function main() {
     return _.merge(...(await Promise.allSettled(tweetIdsChunks.map(tweetIdChunks =>
       axios.get('https://api.twitter.com/1.1/statuses/lookup.json?id=' + encodeURIComponent(tweetIdChunks.join(',')) + "&tweet_mode=extended&map=true&include_ext_alt_text=true&trim_user=true",
         { headers: { 'Authorization': 'Bearer ' + config.twitterApiToken } }).then(res => res.data.id)
-    ))).map(promise => promise.status == 'fulfilled' ? promise.value : null).flat())
+    ))).map(promise => promise._settledValueField))
   }
 
   meta.grab_tweet_data_start = Date.now()
@@ -134,6 +149,9 @@ async function main() {
 
   meta.media = mediaUrls
 
+  meta.user = await axios.get('https://api.twitter.com/1.1/users/show.json?screen_name=' + encodeURIComponent(username),
+   { headers: { 'Authorization': 'Bearer ' + config.twitterApiToken } }).then(res => res.data)
+
   const outputFileName = username + '_' + splitParentUrl[5] + '_' + meta.scrape_ids_start
 
   await fs.mkdir('output/' + outputFileName)
@@ -152,11 +170,11 @@ async function main() {
       const urlSplit = (new URL(media.url)).pathname.split('/')
       const savePath = 'output/' + outputFileName + '/media/' + media.id + '-' + urlSplit[urlSplit.length - 1]
       res.data.pipe(fsSync.createWriteStream(savePath))
-      return new Promise((res, rej) => {
-        res.data.on('end', res)
-        res.data.on('error', rej)
+      return new Promise((resolve, reject) => {
+        res.data.on('end', resolve)
+        res.data.on('error', reject)
       })
-    }), { concurrency: 5 })
+    }), { concurrency: 6 })
   }
   console.log('media downloaded')
 
